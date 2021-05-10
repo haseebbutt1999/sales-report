21de9f8e2adef6b65c086fa5a35dc1d60de1e524
haseebtanveer1999 <haseeb@tetralogicx.com>
2021-05-10 14:45:57 +0500

    apply filter according to date picker and stor location

--- resources/views/adminpanel/dashboard.blade.php
+++ resources/views/adminpanel/dashboard.blade.php
@@ -324,6 +324,84 @@
                 </div>
             </div>
         </form>
+        <div class="w-100 bg-white mt-3">
+            <div class="row " >
+                <div class="col-md-6 pl-3 pt-2 " style="margin-top: -10px;">
+                    <div class="card" style="width: 100%">
+                        <div class="card-header" style="background: white;">
+                            <div class="row ">
+                                @if (session('status'))
+                                    <div class="alert alert-success">
+                                        {{ session('status') }}
+                                    </div>
+                                @endif
+                                <div class="col-md-12 px-3 pt-2">
+                                    <div class="d-flex justify-content-between align-items-center mr-2">
+                                        <h5>Daily Cash Closing:</h5>
+                                        {{--                                    <a href=""><button class="btn-primary">Customer Sync</button></a>--}}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card-body">
+                            <div id="product_append">
+                                <div class="row px-3" style="overflow-x:auto;">
+
+                                    <table id="datatabled" class="table   table-hover  table-class ">
+                                        <tbody>
+                                        <tr class="td-text-center ">
+                                            <td>TOTAL SALE:</td>
+                                            <td>210</td>
+                                        </tr>
+                                        <tr class="td-text-center ">
+                                            <td>CASH SALE:</td>
+                                            <td>100</td>
+                                        </tr>
+                                        <tr class="td-text-center ">
+                                            <td>COMISION %:</td>
+                                            <td>4.75</td>
+                                        </tr>
+                                        <tr class="td-text-center ">
+                                            <td>Payment 1</td>
+                                            <td>0</td>
+                                        </tr>
+                                        <tr class="td-text-center ">
+                                            <td>Payment 2</td>
+                                            <td>0</td>
+                                        </tr>
+                                        <tr class="td-text-center ">
+                                            <td>Payment 3</td>
+                                            <td>0</td>
+                                        </tr>
+                                        <tr class="td-text-center ">
+                                            <td>Payment 4</td>
+                                            <td>0</td>
+                                        </tr>
+                                        <tr class="td-text-center ">
+                                            <td>Payment 5</td>
+                                            <td>0</td>
+                                        </tr>
+                                        <tr class="td-text-center ">
+                                            <td>Total cash remaining $</td>
+                                            <td>95.25</td>
+                                        </tr>
+                                        <tr class="td-text-center ">
+                                            <td>Total cash colected $</td>
+                                            <td>95.25</td>
+                                        </tr>
+
+                                        </tbody>
+                                    </table>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 
 @endsection
